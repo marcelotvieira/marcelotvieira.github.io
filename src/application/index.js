55aa9fb3703558ae981684628aa20d926d929445
marcelovi <marcelodefy15@gmail.com>
2022-06-30 19:47:48 -0300

    Sessão de portfolios alterada para se construir a partir e um JSON.

--- src/application/index.js
+++ src/application/index.js
@@ -1,5 +1,6 @@
 let home = document.getElementById('home');
 let portfolio = document.getElementById('portfolio');
+let portfolioContainer = document.getElementById('portfolio-container')
 let conteudo = document.getElementById('conteudo');
 let sobre = document.getElementById('sobre');
 
@@ -36,5 +37,54 @@ function referenciaRemove(id){
     let page = document.getElementById(id);
     page.classList.remove('active');
 }
+
+
+
+
+    const materials = fetch("../../data/materials.json")
+  .then((resposta) => {
+    return resposta.json();
+  })
+  .then((jsonData) => {
+    let materials = jsonData.materials;
+    for (let material of materials) {
+        const card = document.createElement('div');
+        card.className = 'material-card';
+        const cardImage = document.createElement('img');
+        cardImage.src = material.image;
+
+        const materialInfo = document.createElement('div');
+        materialInfo.className = 'material-info';
+        const materialName = document.createElement('h3');
+        materialName.append(material.name)
+        const materialDescription = document.createElement('p');
+        materialDescription.append(material.description);
+        
+        const sourceButton = document.createElement('button');
+        sourceButton.addEventListener('click', () => {
+            window.open(material.source, '_blank');
+        });
+        
+        sourceButton.append('Visitar')
+        const githubButton = document.createElement('a');
+        githubButton.href = 'https://github.com/marcelotvieira/calculator-app';
+        githubButton.target = '_blank'
+        githubButton.innerHTML = '<i class="fa-brands fa-github fa-2x"></i>'
+        
+        materialInfo.appendChild(materialName)
+        materialInfo.appendChild(materialDescription)
+        materialInfo.appendChild(githubButton)
+        materialInfo.appendChild(sourceButton)
+        
+
+
+        card.append(materialInfo, cardImage)
+        portfolioContainer.append(card);
+
+    }
+
+  })
+
+
 
 
