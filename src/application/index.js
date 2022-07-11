e5d1bfd501f485122e8a77bf184add5d315cd8e7
marcelovi <marcelodefy15@gmail.com>
2022-07-11 11:28:05 -0300

    adição de materiais

--- src/application/index.js
+++ src/application/index.js
@@ -7,17 +7,13 @@ let sobre = document.getElementById('sobre');
 function toggleOption(id) {
     let option = document.getElementById(id);
     option.classList.toggle("active");
-    console.log(id)
 }
 
 function referenciaAdd(id){
     let referencia = '';
     for(let i = 0; id[i] !='-'; i++){
-
         id[i]!='-'?referencia += id[i]:null;  
-
     }
-    console.log(referencia)
     let option = document.getElementById(referencia);
     option.classList.add("active");
     let page = document.getElementById(id);
@@ -27,64 +23,59 @@ function referenciaAdd(id){
 function referenciaRemove(id){
     let referencia = '';
     for(let i = 0; id[i] !='-'; i++){
-
         id[i]!='-'?referencia += id[i]:null;  
-
     }
-    console.log(referencia)
     let option = document.getElementById(referencia);
     option.classList.remove("active");
     let page = document.getElementById(id);
     page.classList.remove('active');
 }
 
-
-
-
-    const materials = fetch("../../data/materials.json")
-  .then((resposta) => {
+const materials = fetch("../../data/materials.json").then((resposta) => {
     return resposta.json();
-  })
-  .then((jsonData) => {
+  }).then((jsonData) => {
     let materials = jsonData.materials;
     for (let material of materials) {
+        /* Construir os elementos */
         const card = document.createElement('div');
         card.className = 'material-card';
         const cardImage = document.createElement('img');
         cardImage.src = material.image;
-
         const materialInfo = document.createElement('div');
         materialInfo.className = 'material-info';
         const materialName = document.createElement('h3');
-        materialName.append(material.name)
         const materialDescription = document.createElement('p');
-        materialDescription.append(material.description);
-        
         const sourceButton = document.createElement('button');
-        sourceButton.addEventListener('click', () => {
-            window.open(material.source, '_blank');
-        });
-        
-        sourceButton.append('Visitar')
         const githubButton = document.createElement('a');
+        /* Aplicar o conteúdo */
+        materialName.append(material.name)
+        materialDescription.append(material.description);
+        sourceButton.addEventListener('click', () => { window.open(material.source, '_blank')});
+        sourceButton.append('Visitar')
         githubButton.href = material.github;
         githubButton.target = '_blank'
         githubButton.innerHTML = '<i class="fa-brands fa-github fa-"></i>'
-        
+        /* Montar o elemento */
         materialInfo.appendChild(materialName)
         materialInfo.appendChild(materialDescription)
         materialInfo.appendChild(githubButton)
         materialInfo.appendChild(sourceButton)
-        
-
-
+        /*Inserir o elemento*/
         card.append(materialInfo, cardImage)
         portfolioContainer.append(card);
-
     }
-
   })
 
+
+const showLoadingAnimation = () => {
+    
+
+}
+
+
+const main = () => {
+
+}
 
 
 
